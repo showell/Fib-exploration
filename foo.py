98b27a0924b3519af8fcbc3b115424a0bee87638
root <showell30@yahoo.com>
2023-03-01 17:57:25 +0000

    Try to make clunky algorithm more efficient.

--- foo.py
+++ foo.py
@@ -7,7 +7,14 @@ for i in range(1, 21):
     if i <= 3:
         val = f[i]
     else:
+        # More than double the population from two months ago.
         val = 2 * f[i-2] + f[i-3]
     f[i] = val
+
+    # garbage collect
+    if i - 3 in f:
+        del f[i-3]
+
+    assert len(f) <= 3
 
     print(f"f({i: 3d}) = {val: 5d}")
