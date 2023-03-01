4de98f51fa0c13f04cc239dc0f5057e1bfebe3a3
root <showell30@yahoo.com>
2023-03-01 19:05:49 +0000

    Introduce "mature_rabbits".

--- foo.py
+++ foo.py
@@ -4,6 +4,7 @@ mama_rabbits = 0
 
 N = 20
 for i in range(1, N+1):
-    total_rabbits = baby_rabbits + teenage_rabbits + mama_rabbits
+    mature_rabbits = teenage_rabbits + mama_rabbits
+    total_rabbits = baby_rabbits + mature_rabbits
     print(f"f({i : 3d}) = {total_rabbits}")
-    (baby_rabbits, teenage_rabbits, mama_rabbits) = (teenage_rabbits + mama_rabbits, baby_rabbits, teenage_rabbits + mama_rabbits)
+    (baby_rabbits, teenage_rabbits, mama_rabbits) = (mature_rabbits, baby_rabbits, mature_rabbits)
