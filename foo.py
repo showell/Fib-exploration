805f7b2a502d91602a4760162c4eb5f52728bad2
root <showell30@yahoo.com>
2023-03-01 15:27:47 +0000

    Old is old.  Stop tracking ages over 2 months.

--- foo.py
+++ foo.py
@@ -5,11 +5,18 @@ def breed(generation_counts):
     baby_count = sum(cnt for cnt in generation_counts[2:])
     return [baby_count] + generation_counts[1:]
 
+def compress(generation_counts):
+    if len(generation_counts) < 3:
+        return generation_counts
+
+    return [generation_counts[0], generation_counts[1],  sum(generation_counts[2:])]
+
 def next_gen(generation_counts):
     return breed(grow(generation_counts))
 
 generation_counts = [1]
 
 for i in range(20):
+    generation_counts = compress(generation_counts)
     print(sum(generation_counts), generation_counts)
     generation_counts = next_gen(generation_counts)
