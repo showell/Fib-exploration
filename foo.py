c6979f3936dfa4b6498bc489095697cca80e87e2
root <showell30@yahoo.com>
2023-03-01 16:56:54 +0000

    Show formulas more clearly.

--- foo.py
+++ foo.py
@@ -1,7 +1,8 @@
 (babies, teenagers, mamas) = (1, 0, 0)
 
-for i in range(20):
-    print((babies + teenagers + mamas), (babies, teenagers, mamas))
+for i in range(1, 21):
+    val = babies + teenagers + mamas
+    print(f"f({i: 3d}) = {val: 5d} females = {babies: 5d} babies + {teenagers: 5d} teenagers + {mamas: 5d} mamas")
     # grow
     (babies, teenagers, mamas) = (0, babies, teenagers + mamas)
     # breed
