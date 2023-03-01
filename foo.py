a46b7bf9639ca81b5e0bcab90be982af318d442e
root <showell30@yahoo.com>
2023-03-01 16:24:56 +0000

    Inline all the functions and use a tuple.

--- foo.py
+++ foo.py
@@ -1,22 +1,8 @@
-def grow(generation_counts):
-    return [0] + generation_counts
-
-def breed(generation_counts):
-    baby_count = sum(cnt for cnt in generation_counts[2:])
-    return [baby_count] + generation_counts[1:]
-
-def compress(generation_counts):
-    if len(generation_counts) < 3:
-        return generation_counts
-
-    return [generation_counts[0], generation_counts[1],  sum(generation_counts[2:])]
-
-def next_gen(generation_counts):
-    return breed(grow(generation_counts))
-
-generation_counts = [1]
+(babies, teenagers, mamas) = (1, 0, 0)
 
 for i in range(20):
-    generation_counts = compress(generation_counts)
-    print(sum(generation_counts), generation_counts)
-    generation_counts = next_gen(generation_counts)
+    print((babies + teenagers + mamas), (babies, teenagers, mamas))
+    # grow
+    (babies, teenagers, mamas) = (0, babies, teenagers + mamas)
+    # breed
+    (babies, teenagers, mamas) = (mamas, teenagers, mamas)
