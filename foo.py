22ad7c52879025ff8ede0e0fca0824147b5a1113
root <showell30@yahoo.com>
2023-03-01 17:51:32 +0000

    Use inductive property observed from last output.

--- foo.py
+++ foo.py
@@ -1,9 +1,13 @@
-(babies, teenagers, mamas) = (1, 0, 0)
+f = dict()
+f[1] = 1
+f[2] = 1
+f[3] = 2
 
 for i in range(1, 21):
-    val = babies + teenagers + mamas
-    print(f"f({i: 3d}) = {val: 5d} females = {babies: 5d} babies + {teenagers: 5d} teenagers + {mamas: 5d} mamas")
-    # grow
-    (babies, teenagers, mamas) = (0, babies, teenagers + mamas)
-    # breed
-    (babies, teenagers, mamas) = (mamas, teenagers, mamas)
+    if i <= 3:
+        val = f[i]
+    else:
+        val = 2 * f[i-2] + f[i-3]
+    f[i] = val
+
+    print(f"f({i: 3d}) = {val: 5d}")
