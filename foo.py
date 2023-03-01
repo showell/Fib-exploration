3edcf5ee4e28ba5d1d40edec4733ed5c000eefb1
root <showell30@yahoo.com>
2023-03-01 18:28:45 +0000

    Convince ourselves of cobminatorical identities:
    Given f(1)=1, f(2)=1, and f(3)=2, as well as
    
        f(N) = 2 * f(N-2) + f(N-3) (for N>=4)
    
    Then for N>=3 it must be that:
    
        f(N) = f(N-1) + f(N-2)

--- foo.py
+++ foo.py
@@ -3,7 +3,8 @@ f[1] = 1
 f[2] = 1
 f[3] = 2
 
-for i in range(1, 21):
+N = 10000
+for i in range(1, N+1):
     if i <= 3:
         val = f[i]
     else:
@@ -11,10 +12,14 @@ for i in range(1, 21):
         val = 2 * f[i-2] + f[i-3]
     f[i] = val
 
+    if i >= 3:
+        assert f[i] == f[i-1] + f[i-2]
+
+    if i >= 4:
+        assert f[i-1] + f[i-2] == 2 * f[i-2] + f[i-3]
+
     # garbage collect
     if i - 3 in f:
         del f[i-3]
 
-    assert len(f) <= 3
-
-    print(f"f({i: 3d}) = {val: 5d}")
+print(f"Identities hold up to N={N}")
