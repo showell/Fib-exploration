24cd384bcb508eca5aebbb6e11e40883cecc770a
root <showell30@yahoo.com>
2023-03-01 18:55:33 +0000

    Go back to rabbits.

--- foo.py
+++ foo.py
@@ -1,17 +1,12 @@
-f = dict()
-f[1] = 1
-f[2] = 1
+baby_rabbits = 1
+teenage_rabbits = 0
+mama_rabbits = 0
 
 N = 20
 for i in range(1, N+1):
-    if i <= 2:
-        val = f[i]
-    else:
-        # Population is sum of two prior generations's populations. But why?
-        val = f[i-1] + f[i-2]
-    f[i] = val
-    print(f"f({i : 3d}) = {val}")
-
-    # garbage collect
-    if i - 2 in f:
-        del f[i-2]
+    total_rabbits = baby_rabbits + teenage_rabbits + mama_rabbits
+    print(f"f({i : 3d}) = {total_rabbits}")
+    # grow rabbits
+    (baby_rabbits, teenage_rabbits, mama_rabbits) = (0, baby_rabbits, teenage_rabbits + mama_rabbits)
+    # breed
+    baby_rabbits = mama_rabbits
