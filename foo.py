9be203e719bd58e35eda8ffd98ee65db7edd8d2b
root <showell30@yahoo.com>
2023-03-01 14:12:37 +0000

    Assume slower rabbit growth.

--- foo.py
+++ foo.py
@@ -1,10 +1,10 @@
 rabbit_pairs = [0]
 
-for i in range(6):
+for i in range(9):
     print(f"{len(rabbit_pairs): 3d}", rabbit_pairs)
     # grow
     rabbit_pairs = [age + 1 for age in rabbit_pairs]
 
     # breed
-    baby_pairs = [0 for age in rabbit_pairs]
+    baby_pairs = [0 for age in rabbit_pairs if age >= 2]
     rabbit_pairs = rabbit_pairs + baby_pairs
